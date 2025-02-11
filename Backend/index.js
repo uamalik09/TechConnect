6d4f51c7aa01dc00bbac37d46c2d45abf3d630d7
Padmavati-123 <padmavatimkudal@gmail.com>
2025-02-11 22:29:10 +0530

    added new files

--- Backend/index.js
+++ Backend/index.js
@@ -3,6 +3,7 @@ const app = express();
 const bodyParser = require('body-parser');
 const cors = require('cors');
 const AuthRouter = require('./Routes/AuthRouter');
+const ProductRouter = require('./Routes/ProductRouter');
 require('dotenv').config();
 require('./Models/db');
 const PORT = process.env.PORT || 8080;
@@ -16,6 +17,7 @@ app.use(bodyParser.json());
 app.use(cors());
 
 app.use('/auth', AuthRouter);
+app.use('/products', AuthRouter);
 
 app.listen(PORT, () => {
     console.log("Server is listening");
--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,14 +1,19 @@
-// import { useState } from 'react'
-// import reactLogo from './assets/react.svg'
-// import viteLogo from '/vite.svg'
-// import './App.css'
+import { Routes, Route, Navigate } from "react-router-dom";
+import Login from "./Components/Login";
+import Signup from "./Components/Signup";
+import Home from "./Components/Home";
 
 function App() {
-  return(
-    <div>
-      hello
+  return (
+    <div className="App">
+      <Routes>
+        <Route path="/" element={<Navigate to="/home" />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<Signup />} />
+        <Route path="/home" element={<Home />} />
+      </Routes>
     </div>
   );
 }
 
-export default App
+export default App;
